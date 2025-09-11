a65b6c1c1d0829956c986c2771a22bf439f33be9
Eduardo <dj.shadowjeab@gmail.com>
2025-09-10 22:29:13 -0500

    menu

---
+++ 01-HTML&CSS/05-proyectoFinal/js/app.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.nav__contenedor__menu');
+const btnMenu = document.querySelector('.nav__contenedor__iconMenuBox');
+
+btnMenu.addEventListener('click', function(){
+    menu.classList.toggle('active');
+})
