4c30543703168681d7f633da574732a154debb5e
Eduardo <dj.shadowjeab@gmail.com>
2025-09-19 21:08:05 -0500

    cap3

--- 02-Javascript/capitulo-02/js/codigo.js
+++ 02-Javascript/capitulo-02/js/codigo.js
@@ -1,4 +1,3 @@
-/* 💥💥 LA FORMA INCORRECTA  💥💥 */
 // 1️⃣ manipular el dato del input
 const tareaInput = document.querySelector('.tarea');
 
@@ -7,15 +6,39 @@ const btn = document.querySelector('button');
 
 // 5️⃣ Manipular la lista
 const lista = document.querySelector('ul');
-// console.log(lista);
+const alerta = document.querySelector('.alerta');
 
 // 3️⃣ agregar un listener al boton para escuchar el evento click
 btn.addEventListener('click', function (){
-    // console.log(tareaInput.value);
     // 4️⃣ inserta a la lista
-    const li = `<li>${tareaInput.value}</li>`;
-    // lista.innerHTML += li;
-    lista.insertAdjacentHTML('beforeend', li);
-    tareaInput.value = '';
+    if(tareaInput.value === '') {
+        alerta.textContent = 'Debes agregar una tarea';
+    } else {
+        const li = `<li>${tareaInput.value}</li>`;
+        lista.insertAdjacentHTML('beforeend', li);
+        tareaInput.value = '';
+        alerta.textContent = '';
+    }
 });
 
+/* 💥💥 LA FORMA INCORRECTA  💥💥 */
+// const tareasInsertadas = document.querySelectorAll('li');
+// // console.log(tareasInsertadas);
+// for(let i = 0; i < tareasInsertadas.length; i++){
+//     tareasInsertadas[i].addEventListener('click', function(){
+//         tareasInsertadas[i].remove();
+//         // this.remove();
+//         // console.log('Diste click en la tarea');
+//     });
+// }
+
+/* 🌟🌟 LA FORMA CORRECTA 🌟🌟 */
+
+lista.addEventListener('click', function(evento){
+    // console.log(evento)
+    // console.log(evento.target.tagName);
+    if(evento.target.tagName === 'LI') {
+        // this.remove();
+        evento.target.remove();
+    }
+});
